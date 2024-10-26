fb7c1d9b99a792b4e79d1d6fa9dbcce0f13d9970
Fredd308 <117744420+Fredd308@users.noreply.github.com>
2024-10-25 21:47:15 -0600

    Primera conexión a base de datos

---
+++ src/pruebamysql/PruebaMySQL.java
@@ -0,0 +1,14 @@
+package pruebamysql;
+
+/**
+ *
+ * @author fredy
+ */
+public class PruebaMySQL {
+
+    public static void main(String[] args) {
+        dbConnection dbc = new dbConnection();
+        dbc.conectar();
+    }
+    
+}
---
+++ src/pruebamysql/dbConnection.java
@@ -0,0 +1,24 @@
+package pruebamysql;
+import java.sql.*;
+
+/**
+ *
+ * @author fredy
+ */
+public class dbConnection {
+    static String url = "jdbc:mysql://localhost:3306/ejemplo";
+    static String user = "root";
+    static String password = "";
+    
+    public static Connection conectar(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexion Exitosa");
+        }
+        catch(SQLException e){
+           e.printStackTrace();
+        }
+        return con;
+    }
+}
