12073695d585fbbf9a444de6584c57be7c4724e9
Fredd308 <117744420+Fredd308@users.noreply.github.com>
2024-10-26 09:36:20 -0600

    se agrego un tercer saludo

--- src/pruebamysql/PruebaMySQL.java
+++ src/pruebamysql/PruebaMySQL.java
@@ -9,6 +9,7 @@ public class PruebaMySQL {
     public static void main(String[] args) {
         System.out.println("HLA PUTAS");
         System.out.println("sdfsdfg");
+        System.out.println("Hola a todos");
         dbConnection dbc = new dbConnection();
         dbc.conectar();
     }
