ee63e40638a3d1ee9525ebadb44115ea69aef79c
Fredd308 <117744420+Fredd308@users.noreply.github.com>
2024-10-25 22:12:11 -0600

    Se agregó un saludo inicial

--- src/pruebamysql/PruebaMySQL.java
+++ src/pruebamysql/PruebaMySQL.java
@@ -7,6 +7,7 @@ package pruebamysql;
 public class PruebaMySQL {
 
     public static void main(String[] args) {
+        System.out.println("Hola a Todos :D");
         dbConnection dbc = new dbConnection();
         dbc.conectar();
     }
