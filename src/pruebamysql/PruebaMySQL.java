cbc8d660385f34e585c0ea41a3547ce4cd4d0a70
Fredd308 <117744420+Fredd308@users.noreply.github.com>
2024-10-26 14:51:44 -0600

    Agregúe mi saludo BYRON

--- src/pruebamysql/PruebaMySQL.java
+++ src/pruebamysql/PruebaMySQL.java
@@ -9,6 +9,7 @@ public class PruebaMySQL {
     public static void main(String[] args) {
         System.out.println("Hola a todos");
         System.out.println("Hola soy Froilan");
+        System.out.println("Hola, soy Byron");
         dbConnection dbc = new dbConnection();
         dbc.conectar();
     }
