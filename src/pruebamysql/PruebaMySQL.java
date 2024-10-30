dc78994226836db52343d49a540a47914d2b0f41
Fredd308 <117744420+Fredd308@users.noreply.github.com>
2024-10-29 18:57:23 -0600

    Se eliminaron los saludos

--- src/pruebamysql/PruebaMySQL.java
+++ src/pruebamysql/PruebaMySQL.java
@@ -7,9 +7,6 @@ package pruebamysql;
 public class PruebaMySQL {
 
     public static void main(String[] args) {
-        System.out.println("Hola a todos");
-        System.out.println("Hola soy Froilan");
-        System.out.println("Hola, soy Byron");
         dbConnection dbc = new dbConnection();
         dbc.conectar();
     }
